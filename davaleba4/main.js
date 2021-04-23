c17a9a3d80d5d7b9aa08bcc2d1c54d61ed47d1d6
Your Name <you@example.com>
2021-04-24 03:48:08 +0400

    homeworks

---
+++ davaleba4/main.js
@@ -0,0 +1,94 @@
+// //closure exercise
+// (function() {
+//     const addBread = function(){
+//         console.log("add bread")
+//     };
+//     const soya = function(){
+//         console.log("spread the soya margarine")
+//     };
+//     const jam = function(){
+//         console.log("add jam")
+//     };
+//     window.makeSandwich = function(){
+//         addBread();
+//         soya();
+//         jam();
+//     }
+// })();
+// makeSandwich();
+
+
+
+
+// //Exercise - Lunar Countdown
+// for(var i = 10; i >= 0; i--){
+//     alert(i);
+//     if(i==0){
+//         alert("Blast Off!")
+//     }  
+// }
+
+
+
+
+// //Iterate with a for loop
+// products = ['Cats', 'Cheese', 'Spanners', 'Lemons', 'Candyfloss'];
+// for(var i = 0; i < products.length; i++){
+//     console.log(products[i]);
+// }
+
+
+
+
+// //Pizza Exercise
+// var toppings = ["Cheese", "Ham", "Bits", "Tomatoes"];
+// function makePizza(array){
+//     console.log(`A tasty pizza with ${array[0]} and  ${array[1]} and  ${array[2]} and  ${array[3]}`)
+// }
+// makePizza(toppings);
+
+
+
+
+// //Harder Exercise - Capitalise every element
+// list = ["html", "css", "javascript", "node js", "angular"];
+// listUpperCased = list.map(x => x.toUpperCase());
+// console.log(listUpperCased);
+
+
+
+// //Exercise - Hello World
+// alert('Hello world');
+
+
+
+
+// //Exercise - Weeks in a lifetime
+// var daysInYear = 365.25;
+// var daysInWeek = 7;
+// var yearsInLifetime = 80;
+// console.log(yearsInLifetime*(daysInYear/daysInWeek));
+
+
+
+
+// // Exercise - String Concatenation
+// var greeting = "hello";
+// var name = "world";
+// var greetingName = greeting + " " + name;
+// alert(greetingName.replace(/l/g, '1').replace(/o/g, '0').split('').reverse().join(''));
+
+
+
+// // Exercise - Conditional logic
+// var date = new Date().getHours();
+// if(date>=5 && date<12){
+//     console.log(" Morning ");
+// }else if(date>=12 && date<17){
+//     console.log("Afternoon");
+// }else {
+//     console.log("Night");
+// }
+
+
+
